da06c58024f484e2b05a75b71d7014bc7023661c
CesarxArteaga <ctmces94@gmail.com>
2020-07-15 20:35:42 -0500

    version alpha

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+   
+    var playing = 0; 
+
+    $(function() {
+        //single book
+        $('#mybook').booklet({
+            width:  900,
+        height: 550,
+            arrows: true
+        });
+    });
+
+    $('button').on('click', function () { 
+        var obj = $('#audio')[0]
+        if(playing == 0){
+            obj.play();
+            playing=1;
+        }else{
+            obj.pause();
+            playing=0;
+        }
+         
+    });
+
+});
